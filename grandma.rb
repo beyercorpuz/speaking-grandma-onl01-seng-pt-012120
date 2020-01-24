6a6e0ba387e426a5152cd0ea2d7ac1ffc8232097
Chris Beyer <beyercorpuz@gmail.com>
2020-01-24 09:07:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,8 +15,9 @@
 def speak_to_grandmaclearclear
   
   
-  If Phrase = uppercase    "I LOVE YOU GRANDMA"
-else if Phrase = uppercase&&shout then " NO,NOT SINCE 1938!"
+  If Phrase = uppercase   
+  return "I LOVE YOU GRANDMA"
+else if clear = uppercase&&shout then " NO,NOT SINCE 1938!"
   ELSE IF Phrase = exactly "I LOVE YOU TOO PUMPKIN!"
   
   END
